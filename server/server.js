85aac6504207129b72525804c678a8804780806c
Ahmed Skopljak <ahmed.skopljak.23@size.ba>
2026-02-25 01:28:24 +0100

    Gitignore, user schema, connection with database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import "dotenv/config"
 
 const app = express();
 const port = process.env.PORT || 4000;
+const allowedOrigins = ['http://localhost:5173']
+
+await connectDB();
+
+//Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get("/", (req, res) => res.send("API Is Working"));
 app.listen(port, () => {
