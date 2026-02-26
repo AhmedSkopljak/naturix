4426750f7664a92ea5540e9216b75035f250d8ef
Ahmed Skopljak <ahmed.skopljak.23@size.ba>
2026-02-26 15:16:52 +0100

    Backend Done

---
+++ server/contollers/orderController.js
@@ -0,0 +1,61 @@
+import Product from "../models/Product.js";
+import Order from "../models/Order.js";
+
+//Place order COD: /api/order/cod
+export const placeOrderCOD = async (req, res) => {
+    try{
+        const {userId, items, address} = req.body;
+        if (!address || items.length === 0) {
+            return res.json({success: false, message: "Invalid Data"})
+        }
+
+        //Calculate amount using items
+        let amount = await items.reduce(async (acc, item) => {
+            const product = Product.findById(item.product);
+            return (await acc) + product.offerPrice * item.quantity;
+        }, 0);
+
+        //Add tax charge 2%
+        amount += Math.floor(amount * 0.02);
+
+        await Order.create({
+            userId,
+            items,
+            amount,
+            address,
+            paymentType: "COD",
+        });
+
+        return res.json({success: true, message: "Order created successfully"})
+    }catch(error){
+        return res.json({success: false, message: error.message})
+    }
+}
+
+//Get orders by userId: /api/order/user
+export const getUserOrders = async (req, res) => {
+    try{
+        const {userId} = req.body;
+        const orders = await Order.find({
+            userId,
+            $or: [{paymentType: "COD"}, {isPaid: true}]
+        }).populate("items.product address").sort({ createdAt: -1 })
+
+        res.json({success:true, orders})
+    }catch(error){
+        res.json({success: false, message: error.message})
+    }
+}
+
+//Get all orders (admin): /api/order/seller
+export const getAllOrders = async (req, res) => {
+    try{
+        const orders = await Order.find({
+            $or: [{paymentType: "COD"}, {isPaid: true}]
+        }).populate("items.product address").sort({ createdAt: -1 })
+
+        res.json({success:true, orders})
+    }catch(error){
+        res.json({success: false, message: error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import sellerRouter from "./routes/sellerRoute.js";
 import productRouter from "./routes/productRoute.js";
 
 import cartRuter from "./routes/cartRute.js";
+import addressRouter from "./routes/addressRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -23,8 +25,10 @@ app.use(cors({origin: allowedOrigins, credentials: true}));
 app.get("/", (req, res) => res.send("API Is Working"));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
-app.use("/api/product/", productRouter);
+app.use("/api/product", productRouter);
 app.use("/api/cart", cartRuter)
+app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
