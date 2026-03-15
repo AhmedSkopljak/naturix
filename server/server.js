df1f377722f6ec77ccc6067ad8f285454ff6ea40
Ahmed Skopljak <ahmed.skopljak.23@size.ba>
2026-03-16 00:43:48 +0100

    small fix

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ import "dotenv/config"
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
 import productRouter from "./routes/productRoute.js";
-
 import cartRuter from "./routes/cartRute.js";
 import addressRouter from "./routes/addressRoute.js";
 import orderRouter from "./routes/orderRoute.js";
@@ -14,25 +13,27 @@ import {stripeWebhooks} from "./contollers/orderController.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173'];
 
 await connectDB();
 
-app.post("/stripe", express.raw({type: "application/json"}), stripeWebhooks)
+// Stripe webhook mora biti PRIJE express.json() middlewarea
+// jer treba raw body za verifikaciju potpisa
+app.post("/stripe", express.raw({type: "application/json"}), stripeWebhooks);
 
-//Middleware configuration
+// Middleware configuration
+app.use(cors({origin: allowedOrigins, credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get("/", (req, res) => res.send("API Is Working"));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
-app.use("/api/cart", cartRuter)
+app.use("/api/cart", cartRuter);
 app.use("/api/address", addressRouter);
 app.use("/api/order", orderRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-})
+});
